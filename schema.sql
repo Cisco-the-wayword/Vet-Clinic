12d276230a1c4106bbb1de146226e2f4d3d109c0
Cisco <ciscophrenzy@gmail.com>
2023-06-15 07:34:03 -0600

    Add other tables and implement JOIN to query data

--- schema.sql
+++ schema.sql
@@ -9,3 +9,24 @@ CREATE TABLE ANIMALS(
    WEIGHT_KG DECIMAL,
    SPECIES VARCHAR(255)
 );
+CREATE TABLE ANIMALS (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    species_id INTEGER REFERENCES species(id),
+    owner_id INTEGER REFERENCES owners(id)
+);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR,
+    age INT
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
+);
